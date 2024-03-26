206af46cd1f33d16385a696161af6f013ce06a3e
davidbuckleyni <davidbuckleyweb@gmail.com>
2024-03-26 14:59:03 +0000

    Add project files.

---
+++ BookManager.API/BookManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManager.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookManager : ControllerBase
+    {
+
+
+    }
+}
---
+++ BookManager.Services/BookManagerServices.cs
@@ -0,0 +1,18 @@
+﻿using BookManager.Services.Interfaces;
+
+namespace BookManager.Services
+{
+    public class BookManagerServices : IBookManagerServices
+    {
+        IBookManagerServices _ibookManagerServices;
+        public BookManagerServices(IBookManagerServices ibookManagerServices) 
+        
+        {
+            _ibookManagerServices = ibookManagerServices;
+        }
+        public void AddAuthor()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
